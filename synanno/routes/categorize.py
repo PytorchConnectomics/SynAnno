64faffd7c943297a9b74fe74706374df573b9dfc
Leander Lauenburg <leander.lauenburg@gmail.com>
2022-05-13 15:47:33 -0400

    edited layout, implemented flag processing

--- synanno/routes/categorize.py
+++ synanno/routes/categorize.py
@@ -1,12 +1,28 @@
-from flask import render_template, session
+from crypt import methods
+from flask import render_template, session, request, Response
 
 # import the package app 
 from synanno import app
-
-import os
-import json
+from flask_cors import cross_origin
 
 
 @app.route('/categorize')
 def categorize():
-    return render_template('categorize.html', images=session.get('data')[0], page=0, n_pages=session.get('n_pages'))
+    return render_template('categorize.html', pages=session.get('data'))
+
+
+@app.route('/pass_flags', methods=['POST'])
+@cross_origin()
+def pass_flags():
+    flag = request.form.getlist('flag[]')
+    print("Debug")
+    print(flag)
+    
+    data = session.get('data')
+
+    page_nr, img_nr, f = flag
+    data[int(page_nr)][int(img_nr)]['Error_Description'] = str(f)
+
+    session['data'] = data
+    print(session['data'])
+    return Response(status = 200)
--- synanno/static/categorization.js
+++ synanno/static/categorization.js
@@ -1,43 +1,59 @@
 $(document).ready(function(){
 
-    $('#submit_button').click(function(){
-        $('[id^="id_error_"]').each(function() {
-            if ($("#falsePositive", this).is(":checked")) {
-                console.log("falsePositive")
-              }
-            else if ($("#badFit", this).is(":checked")) {
-                console.log("badFit")
-            }
-            else if ($("#polaritySwitch", this).is(":checked")) {
-                console.log("polaritySwitch")
-            }
-            else if ($('[id^="customFlagButton_"]', this).is(":checked")) {
-                console.log($('[id^="customFlagInput_"]', this).val())
-            }
-        });
-    });
-
+    // ensure that the text field is only enabled if the button is selected
     $('[id^="customFlagButton_"]').change(function() {
-        var nr = $(this).attr('id').replace(/customFlagButton_/, '');
-        var customFlagId = '#customFlagInput_'+nr
+        var [page, img_id] = $(this).attr('id').replace(/customFlagButton_/, '').split('_')
+        var customFlagId = '#customFlagInput_'+page+'_'+img_id
         $(customFlagId).removeAttr('disabled');
      });
 
      $('[id^="falsePositive_"]').change(function() {
-        var nr = $(this).attr('id').replace(/falsePositive_/, '');
-        var customFlagId = '#customFlagInput_'+nr
+        var [page, img_id] = $(this).attr('id').replace(/falsePositive_/, '').split('_')
+        var customFlagId = '#customFlagInput_'+page+'_'+img_id
         $(customFlagId).attr('disabled','disabled');
      });
 
      $('[id^="badFit_"]').change(function() {
-        var nr = $(this).attr('id').replace(/badFit_/, '');
-        var customFlagId = '#customFlagInput_'+nr
+        var [page, img_id] = $(this).attr('id').replace(/badFit_/, '').split('_')
+        var customFlagId = '#customFlagInput_'+page+'_'+img_id
         $(customFlagId).attr('disabled','disabled');
      });
 
      $('[id^="polaritySwitch_"]').change(function() {
-        var nr = $(this).attr('id').replace(/polaritySwitch_/, '');
-        var customFlagId = '#customFlagInput_'+nr
+        var [page, img_id] = $(this).attr('id').replace(/polaritySwitch_/, '').split('_')
+        var customFlagId = '#customFlagInput_'+page+'_'+img_id
         $(customFlagId).attr('disabled','disabled');
      });
+
+    // process the flags: [[pager number, image number, flag], ..., [pager number, image number, flag]]
+    $('#submit_button').click(async function(){
+        await $('[id^="id_error_"]').each(function() {
+            var flag = []
+            var [page, img_id] = $(this).attr('id').replace(/id_error_/, '').split('_')
+            flag.push(page)
+            flag.push(img_id)
+            if ($('[id^="falsePositive_"]', this).is(":checked")) {
+                flag.push("falsePositive")
+              }
+            else if ($('[id^="badFit_"]', this).is(":checked")) {
+                flag.push("badFit")
+            }
+            else if ($('[id^="polaritySwitch_"]', this).is(":checked")) {
+                flag.push("polaritySwitch")
+            }
+            else if ($('[id^="customFlagButton_"]', this).is(":checked")) {
+                flag.push($('[id^="customFlagInput_"]', this).val())
+            }
+            else{
+                flag.push("None")
+            }
+            console.log(flag)
+            req = $.ajax({
+                url: '/pass_flags',
+                type: 'POST',
+                data: {flag: flag}
+
+        });
+        });
+    });
 })

942ff94ffaf207110818bdc5a3885443e6194069
Leander Lauenburg <leander.lauenburg@gmail.com>
2022-06-07 13:50:05 -0400

    working version with arc, adjustable thickness, polarity

--- synanno/static/draw.js
+++ synanno/static/draw.js
@@ -9,6 +9,20 @@ $(document).ready(function () {
     var points = []
     var pointsQBez = []
 
+    // polarity colors
+    var turquoise = 'rgba(51, 255, 240, 1)';
+    var pink = 'rgba(252, 14, 249, 1)';
+    
+    var color_1 = turquoise
+    var color_2 = pink
+
+    var color_toggle = 0
+
+    // thickness
+    var thickness = 10
+
+        
+
     // on click activate drawing
     $("#canvasButtonActivate").on("click", function () {
         $("canvas.coveringCanvas").removeClass('d-none')
@@ -23,92 +37,30 @@ $(document).ready(function () {
         points = []
     });
 
+
+    // on click activate drawing
+    $("#canvasButtonPolarity").on("click", function () {
+        if (color_toggle == 0) {
+            color_1 = pink
+            color_2 = turquoise
+            color_toggle = 1
+        } else if (color_toggle == 1) {
+            color_1 = turquoise
+            color_2 = pink
+            color_toggle = 0
+        }
+        fill_clip(color_1, color_2, thickness)
+    });
+
+    // create mask
+    $('input[type=range]').on('input', function () { 
+        thickness = $(this).val()
+        fill_clip(color_1, color_2, thickness, sample=false)
+    });
+
     // create mask
     $("#canvasButtonCreate").on("click", function () {
-        clear_canvas()
-        ctx.beginPath()
-        if (points.length > 2) {
-
-            first = true
-            for (i = 0; i < points.length - 2; i++) {
-                //control.append({x:(points[i+1].x + points[i + 2].x) / 2})
-
-                if (first) {
-                    ax = points[i].x
-                    ay = points[i].y
-                    first = false
-                } else {
-                    ax = bx
-                    ay = by
-                }
-                bx = (points[i + 1].x + points[i + 2].x) / 2
-                by = (points[i + 1].y + points[i + 2].y) / 2
-                cx = points[i + 1].x
-                cy = points[i + 1].y
-
-
-                plotQBez(pointsQBez, 100, 10, ax, ay, cx, cy, bx, by)
-            }
-            plotQBez(pointsQBez, 100, 10, bx, by, points[points.length - 1].x, points[points.length - 1].y, points[points.length - 1].x, points[points.length - 1].y)
-            pointsQBez.push({ x: points[points.length - 1].x, y: points[points.length - 1].y })
-
-            let region = new Path2D();
-
-            for (j = 0; j < pointsQBez.length; j++) {
-                //ctx.beginPath()
-                // the normal aspect ratio of a html5 canvas is 2/1 
-                // since we have equal with and equal height, y is distorted by factor 2
-                //region.ellipse(pointsQBez[j].x, pointsQBez[j].y, 20, 10, 0, 0, Math.PI * 2)
-                region.rect(pointsQBez[j].x-10, pointsQBez[j].y-5, 20, 10);
-                //ctx.stroke()
-
-                //region.rect(pointsQBez[j].x-10, pointsQBez[j].y-10,20,20)
-
-            }
-            ctx.clip(region)
-
-            ctx.beginPath()
-
-            ctx.fillStyle = "#33FFF0";
-            ctx.strokeStyle = "#33FFF0";
-
-            ctx.rect(0, 0, canvas.get(0).width, canvas.get(0).height);
-            //draw_quad_line(points, lineWidth = 1, draw_points = false, close_path = true, topOrBot = 'bot')
-            ctx.fill()
-
-            ctx.fillStyle = "#FC0EF9";
-            ctx.strokeStyle = "#FC0EF9";
-
-            draw_quad_line(points, lineWidth = 1, draw_points = false, close_path = true)
-            ctx.fill()
-
-
-
-            // draw outer line
-            //ctx.strokeStyle = 'rgba(0,0,0,0.7)';
-            //ctx.lineCap = 'round'
-            //draw_quad_line(points, lineWidth = 20, draw_points = false)
-
-        }
-        else if (points.length > 1) {
-            ctx.beginPath()
-            ctx.strokeStyle = "#33FFF0"
-            ctx.moveTo((points_r[0].x), points_r[0].y);
-            ctx.fillRect(points_r[0].x, points_r[0].y, 2, 2)
-            ctx.fillRect(points_r[1].x, points_r[1].y, 2, 2)
-            ctx.lineTo(points_r[0].x, points_r[0].y);
-            ctx.lineTo(points_r[1].x, points_r[1].y);
-            ctx.stroke();
-
-            ctx.beginPath()
-            ctx.strokeStyle = "#FC0EF9"
-            ctx.moveTo((points_l[0].x), points_l[0].y);
-            ctx.fillRect(points_l[0].x, points_l[0].y, 2, 2)
-            ctx.fillRect(points_l[1].x, points_l[1].y, 2, 2)
-            ctx.lineTo(points_l[0].x, points_l[0].y);
-            ctx.lineTo(points_l[1].x, points_l[1].y);
-            ctx.stroke();
-        }
+        fill_clip(color_1, color_2, thickness)
     });
 
     $("canvas.coveringCanvas").on("click", function (e) {
@@ -145,6 +97,86 @@ $(document).ready(function () {
             y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
         };
     }
+
+    function fill_clip(color_1, color_2, thickness, sample=true) {
+        clear_canvas()
+        ctx.beginPath()
+        pl = points.length
+        if (pl > 2) {
+
+            if (sample){
+            ax = points[0].x
+            ay = points[0].y
+            for (i = 1; i < pl - 2; i++) {
+                //control.append({x:(points[i+1].x + points[i + 2].x) / 2})
+                
+                cx = points[i].x
+                cy = points[i].y
+                bx = (points[i].x + points[i + 1].x)/2
+                by = (points[i].y + points[i + 1].y)/2
+
+                plotQBez(pointsQBez, 100, ax, ay, cx, cy, bx, by)
+
+                ax = bx
+                ay = by
+            }
+            bx = points[pl-1].x
+            by = points[pl-1].y
+            cx = points[pl-2].x
+            cy = points[pl-2].y
+            plotQBez(pointsQBez, 100, ax, ay, cx, cy, bx, by)
+
+            //plotQBez(pointsQBez, 100, 15, bx, by, points[points.length - 2].x, points[points.length - 2].y, points[points.length - 1].x, points[points.length - 1].y)
+            //pointsQBez.push({ x: points[points.length - 1].x, y: points[points.length - 1].y })
+        }
+            ctx.save()
+            let region = new Path2D();
+
+
+            for (j = 0; j < pointsQBez.length; j++) {
+                // the normal aspect ratio of a html5 canvas is 2/1 
+                // since we have equal with and equal height, y is distorted by factor 2
+
+                region.moveTo(pointsQBez[j].x + thickness, pointsQBez[j].y)
+                region.ellipse(pointsQBez[j].x, pointsQBez[j].y, thickness, Math.floor(thickness/2), 0, 0, Math.PI * 2)
+            }
+            ctx.clip(region)
+
+            ctx.fillStyle = color_1;
+            ctx.strokeStyle = color_1;
+
+            ctx.rect(0, 0, canvas.get(0).width, canvas.get(0).height);
+            ctx.fill()
+
+            ctx.fillStyle = color_2;
+            ctx.strokeStyle = color_2;
+
+            draw_quad_line(points, lineWidth = 1, draw_points = false, close_path = true)
+            ctx.fill()
+            ctx.restore()
+
+
+        }
+        else if (points.length > 1) {
+            ctx.beginPath()
+            ctx.strokeStyle = "#33FFF0"
+            ctx.moveTo((points_r[0].x), points_r[0].y);
+            ctx.fillRect(points_r[0].x, points_r[0].y, 2, 2)
+            ctx.fillRect(points_r[1].x, points_r[1].y, 2, 2)
+            ctx.lineTo(points_r[0].x, points_r[0].y);
+            ctx.lineTo(points_r[1].x, points_r[1].y);
+            ctx.stroke();
+
+            ctx.beginPath()
+            ctx.strokeStyle = "#FC0EF9"
+            ctx.moveTo((points_l[0].x), points_l[0].y);
+            ctx.fillRect(points_l[0].x, points_l[0].y, 2, 2)
+            ctx.fillRect(points_l[1].x, points_l[1].y, 2, 2)
+            ctx.lineTo(points_l[0].x, points_l[0].y);
+            ctx.lineTo(points_l[1].x, points_l[1].y);
+            ctx.stroke();
+        }
+    };
 
     function draw_quad_line(points, lineWidth = 1, draw_points = true, close_path = false) {
         ctx.beginPath()
@@ -197,8 +229,6 @@ $(document).ready(function () {
         // draw line to intersection of the end path with the border
         ctx.lineTo(int_end.x, int_end.y)
 
-        console.log(int_start.d)
-        console.log(int_end.d)
         if (int_start.d == int_end.d) {
             ctx.lineTo(int_start.x, int_start.y) // if both ends go through the same boarder
         } else if (int_start.d == "bot" && int_end.d == "top") {
@@ -220,13 +250,13 @@ $(document).ready(function () {
         } else if (int_start.d == "top" && int_end.d == "right" || int_start.d == "right" && int_end.d == "top") {
             ctx.lineTo(width, 0) // top right corner
             ctx.lineTo(int_start.x, int_start.y) // intersection
-        }else if (int_start.d == "top" && int_end.d == "left" || int_start.d == "left" && int_end.d == "top") {
+        } else if (int_start.d == "top" && int_end.d == "left" || int_start.d == "left" && int_end.d == "top") {
             ctx.lineTo(0, 0) // top left corner
             ctx.lineTo(int_start.x, int_start.y) // intersection
-        }else if (int_start.d == "bot" && int_end.d == "right" || int_start.d == "right" && int_end.d == "bot") {
+        } else if (int_start.d == "bot" && int_end.d == "right" || int_start.d == "right" && int_end.d == "bot") {
             ctx.lineTo(width, height) // top right corner
             ctx.lineTo(int_start.x, int_start.y) // intersection
-        }else if (int_start.d == "bot" && int_end.d == "left" || int_start.d == "left" && int_end.d == "bot") {
+        } else if (int_start.d == "bot" && int_end.d == "left" || int_start.d == "left" && int_end.d == "bot") {
             ctx.lineTo(0, height) // top right corner
             ctx.lineTo(int_start.x, int_start.y) // intersection
         }
@@ -239,10 +269,9 @@ $(document).ready(function () {
         m = slope(x1, x2, y1, y2)
         b = offset(x1, y1, m)
 
-        console.log(x1, y1, x2, y2)
         // x1---->x2
         if (x2 > x1) {
-            
+
             // intersection with the top border 
             if (y2 < y1) {
                 x = (0 - b) / m
@@ -258,7 +287,7 @@ $(document).ready(function () {
                     return { x: x, y: height, d: "bot" }
                 }
             }
-            
+
             // intersection with the right border
             y = width * m + b
             if (y <= height && y >= 0) {
@@ -278,17 +307,16 @@ $(document).ready(function () {
                 if (x <= width && x >= 0) {
                     return { x: x, y: height, d: "bot" }
                 }
-            } 
+            }
             // intersection with the left border
             y = 0 * m + b
             if (y <= height && y >= 0) {
                 return { x: 0, y: y, d: "left" }
             }
-            
+
         }
 
     }
-
 
 
     function slope(startX, endX, startY, endY) {
@@ -299,30 +327,28 @@ $(document).ready(function () {
         return (y - x * m)
     }
 
-    function plotQBez(pointsQBez, ptCount, pxTolerance, Ax, Ay, Bx, By, Cx, Cy) {
-        var deltaBAx = Bx - Ax;
-        var deltaCBx = Cx - Bx;
-        var deltaBAy = By - Ay;
-        var deltaCBy = Cy - By;
-        var ax, ay;
-        var lastX = -10000;
-        var lastY = -10000;
+    function _getQBezierValue(t, p1, p2, p3) {
+        var iT = 1 - t;
+        return iT * iT * p1 + 2 * iT * t * p2 + t * t * p3;
+    }
+    
+    function getQuadraticCurvePoint(startX, startY, cpX, cpY, endX, endY, position) {
+        return {
+            x:  _getQBezierValue(position, startX, cpX, endX),
+            y:  _getQBezierValue(position, startY, cpY, endY)
+        };
+    }
+
+    function plotQBez(pointsQBez, ptCount, Ax, Ay, Cx, Cy, Bx, By) {
         pointsQBez.push({ x: Ax, y: Ay });
         for (var i = 1; i < ptCount; i++) {
             var t = i / ptCount;
-            ax = Ax + deltaBAx * t;
-            ay = Ay + deltaBAy * t;
-            var x = ax + ((Bx + deltaCBx * t) - ax) * t;
-            var y = ay + ((By + deltaCBy * t) - ay) * t;
-            var dx = x - lastX;
-            var dy = y - lastY;
-            if (dx * dx + dy * dy > pxTolerance) {
-                pointsQBez.push({ x: x, y: y });
-                lastX = x;
-                lastY = y;
-            }
+
+            console.log(t)
+            pointsQBez.push(getQuadraticCurvePoint(Ax, Ay, Cx, Cy, Bx, By, t))
         }
-        pointsQBez.push({ x: Cx, y: Cy });
+        pointsQBez.push({ x: Bx, y: By });
         return (pointsQBez);
-    }
+    };
+
 });

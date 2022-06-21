a792f0541ee47424700d5b0a3313c3fc04baa2f9
Leander Lauenburg <leander.lauenburg@gmail.com>
2022-06-21 16:46:04 -0400

    added circular eraser

--- synanno/static/draw.js
+++ synanno/static/draw.js
@@ -1,42 +1,108 @@
 $(document).ready(function () {
 
+    // retrieve canvas and set 2D context
     const canvas = $("canvas.coveringCanvas");
     const ctx = canvas.get(0).getContext("2d");
+
+    // set red as default color; used for the control points
     ctx.fillStyle = "#FF0000";
 
+    // initialize mouse position to zero
+    var mousePosition = {
+        x: 0,
+        y: 0
+    };
+
+    // init default span variables for the canvas
+    var width;
+    var height;
+
+    // initialize global rectangle variable
     var rect;
 
+    // turn mask drawing and splitting of at the start
+    var draw_mask = false;
+    var split_mask = false;
+
+    // global buffer for the start, end, and control points of the spline
     var points = []
+
+    // global buffer for the point on the curve
     var pointsQBez = []
 
-    // polarity colors
-    var turquoise = 'rgba(51, 255, 240, 1)';
-    var pink = 'rgba(252, 14, 249, 1)';
+    // init thickness
+    var thickness = 10
+
+    // define colors
+    const turquoise = 'rgba(51, 255, 240)';
+    const pink = 'rgba(252, 14, 249)';
     
+    // set polarity default
     var color_1 = turquoise
     var color_2 = pink
 
+    // set variable for toggling the polarity
     var color_toggle = 0
 
-    // thickness
-    var thickness = 10
-
-        
-
-    // on click activate drawing
+    // on click activate canvas
     $("#canvasButtonActivate").on("click", function () {
-        $("canvas.coveringCanvas").removeClass('d-none')
-        rect = $("canvas.coveringCanvas").get(0).getBoundingClientRect() /// get absolute rect. of canvas
-        width = canvas.get(0).width
-        height = canvas.get(0).height
+        // activate canvas for the first time
+        if ($("canvas.coveringCanvas").hasClass('d-none')){
+            $(this).text("Reset Canvas");
+            $("canvas.coveringCanvas").removeClass('d-none') // change visibility
+            rect = $("canvas.coveringCanvas").get(0).getBoundingClientRect() // get absolute rect. of canvas
+            width = canvas.get(0).width // retrieve the width of the canvas
+            height = canvas.get(0).height // retrieve the width of the canvas
+            split_mask = false // deactivate mask splitting
+            draw_mask = true // activate mask drawing
+        // reset canvas
+        }else{
+            clear_canvas()
+            points = []
+            pointsQBez = []
+            split_mask = false // deactivate mask splitting
+            draw_mask = true // activate mask drawing
+        }
     });
 
-    // on click activate drawing
-    $("#canvasButtonDelete").on("click", function () {
-        clear_canvas()
-        points = []
+    // toggle split mode
+    var toggle_split = 0
+
+    // activate event for splitting/erasing the curve
+    $("#canvasButtonSplit").on("click", function () {
+        // activate erasing mode
+        if (toggle_split == 0){
+            ctx.save()
+            ctx.globalCompositeOperation = 'destination-out';
+            split_mask = true;
+        // deactivate erasing mode
+        }else if (toggle_split == 1){
+            split_mask = false;
+            ctx.restore()
+        }
     });
 
+    // if split_mask = true: circular eraser
+    $("canvas.coveringCanvas").mousemove(function( event ){
+        // set global composite operation to destination-out
+        if (split_mask){
+            // detect possible changes in mouse position
+            if ((mousePosition.x != event.clientX  || mousePosition.y != event.clientY) && event.buttons == 1) {
+                mousePosition.x = event.clientX;
+                mousePosition.y = event.clientY;
+
+                // Set global composite operation to destination-out
+                var pos = getXY(this, event)
+                var x = pos.x
+                var y = pos.y
+                ctx.strokeStyle = "#000";
+                ctx.beginPath();
+                ctx.moveTo(x + thickness, y)
+                ctx.ellipse(x, y, thickness, Math.floor(thickness/2), 0, 0, Math.PI * 2)
+                ctx.fill();                    
+            }
+        }
+     })
 
     // on click activate drawing
     $("#canvasButtonPolarity").on("click", function () {
@@ -52,37 +118,62 @@ $(document).ready(function () {
         fill_clip(color_1, color_2, thickness)
     });
 
-    // create mask
-    $('input[type=range]').on('input', function () { 
+    // create the mask based on the drawn spline
+    $("#canvasButtonCreate").on("click", function () {
+        draw_mask = false;
+        fill_clip(color_1, color_2, thickness)
+    });
+
+    // adapt the thickness of the spline
+    $('#thickness_range').on('input', function () { 
         thickness = $(this).val()
         fill_clip(color_1, color_2, thickness, sample=false)
     });
 
-    // create mask
-    $("#canvasButtonCreate").on("click", function () {
-        fill_clip(color_1, color_2, thickness)
-    });
 
     $("canvas.coveringCanvas").on("click", function (e) {
-        clear_canvas()
-        ctx.beginPath()
-        var pos = getXY(this, e)
-        var x = pos.x
-        var y = pos.y
+        if (draw_mask){
+            clear_canvas()
+            ctx.beginPath()
+            var pos = getXY(this, e)
+            var x = pos.x
+            var y = pos.y
 
-        points.push({ x, y })
-        if (points.length > 2) {
-            draw_quad_line(points)
-        }
-        else if (points.length > 1) {
-            ctx.moveTo((points[0].x), points[0].y);
-            ctx.fillRect(points[0].x, points[0].y, 2, 2)
-            ctx.fillRect(points[1].x, points[1].y, 2, 2)
-            ctx.lineTo(points[0].x, points[0].y);
-            ctx.lineTo(points[1].x, points[1].y);
-            ctx.stroke();
+            points.push({ x, y })
+            if (points.length > 2) {
+                draw_quad_line(points)
+            }
+            else if (points.length > 1) {
+                ctx.moveTo((points[0].x), points[0].y);
+                ctx.fillRect(points[0].x, points[0].y, 2, 2)
+                ctx.fillRect(points[1].x, points[1].y, 2, 2)
+                ctx.lineTo(points[0].x, points[0].y);
+                ctx.lineTo(points[1].x, points[1].y);
+                ctx.stroke();
+            }
         }
     })
+
+    function draw_mask(){
+        clear_canvas()
+            ctx.beginPath()
+            var pos = getXY(this, e)
+            var x = pos.x
+            var y = pos.y
+
+            points.push({ x, y })
+            if (points.length > 2) {
+                draw_quad_line(points)
+            }
+            else if (points.length > 1) {
+                ctx.moveTo((points[0].x), points[0].y);
+                ctx.fillRect(points[0].x, points[0].y, 2, 2)
+                ctx.fillRect(points[1].x, points[1].y, 2, 2)
+                ctx.lineTo(points[0].x, points[0].y);
+                ctx.lineTo(points[1].x, points[1].y);
+                ctx.stroke();
+            }
+    }
 
     function clear_canvas() {
         ctx.beginPath();
@@ -125,22 +216,18 @@ $(document).ready(function () {
             cx = points[pl-2].x
             cy = points[pl-2].y
             plotQBez(pointsQBez, 100, ax, ay, cx, cy, bx, by)
+        }            
 
-            //plotQBez(pointsQBez, 100, 15, bx, by, points[points.length - 2].x, points[points.length - 2].y, points[points.length - 1].x, points[points.length - 1].y)
-            //pointsQBez.push({ x: points[points.length - 1].x, y: points[points.length - 1].y })
-        }
-            ctx.save()
-            let region = new Path2D();
+            ctx.save();
 
-
+            let region_1 = new Path2D();
             for (j = 0; j < pointsQBez.length; j++) {
                 // the normal aspect ratio of a html5 canvas is 2/1 
-                // since we have equal with and equal height, y is distorted by factor 2
-
-                region.moveTo(pointsQBez[j].x + thickness, pointsQBez[j].y)
-                region.ellipse(pointsQBez[j].x, pointsQBez[j].y, thickness, Math.floor(thickness/2), 0, 0, Math.PI * 2)
+                // since we have equal width and height, y is distorted by factor 2
+                region_1.moveTo(pointsQBez[j].x + thickness, pointsQBez[j].y)
+                region_1.ellipse(pointsQBez[j].x, pointsQBez[j].y, thickness, Math.floor(thickness/2), 0, 0, Math.PI * 2)
             }
-            ctx.clip(region)
+            ctx.clip(region_1)
 
             ctx.fillStyle = color_1;
             ctx.strokeStyle = color_1;
@@ -149,11 +236,10 @@ $(document).ready(function () {
             ctx.fill()
 
             ctx.fillStyle = color_2;
-            ctx.strokeStyle = color_2;
-
+            ctx.strokeStyle = color_2
             draw_quad_line(points, lineWidth = 1, draw_points = false, close_path = true)
             ctx.fill()
-            ctx.restore()
+            ctx.restore();
 
 
         }
@@ -214,10 +300,22 @@ $(document).ready(function () {
         if (close_path) {
             pl = points.length - 1
 
-            intersection_end = intersection(points[pl - 1].x, points[pl - 1].y, points[pl].x, points[pl].y)
-            intersection_start = intersection(points[1].x, points[1].y, points[0].x, points[0].y)
-            close_intersection(intersection_start, intersection_end)
-            ctx.closePath()
+            // intersection with it self inside the canvas
+            line_intersection = line_intersect(points[pl - 1].x, points[pl - 1].y, points[pl].x, points[pl].y, points[1].x, points[1].y, points[0].x, points[0].y)
+            console.log(line_intersection)
+            if (!line_intersection){
+                console.log("No inter")
+                // intersection with boundary
+                intersection_end = intersection(points[pl - 1].x, points[pl - 1].y, points[pl].x, points[pl].y)
+                intersection_start = intersection(points[1].x, points[1].y, points[0].x, points[0].y)
+                close_intersection(intersection_start, intersection_end)
+                ctx.closePath()
+            }else{
+                console.log("intersection")
+                ctx.lineTo(line_intersection.x, line_intersection.y)
+                ctx.lineTo(points[0].x, points[0].y)
+                ctx.closePath()
+            }
         }
 
         if (!close_path) {
@@ -293,7 +391,7 @@ $(document).ready(function () {
             if (y <= height && y >= 0) {
                 return { x: width, y: y, d: "right" }
             }
-        } else if (x2 < x1) {
+        } else{
             // intersection with the top border 
             if (y2 < y1) {
                 x = (0 - b) / m
@@ -317,7 +415,6 @@ $(document).ready(function () {
         }
 
     }
-
 
     function slope(startX, endX, startY, endY) {
         return (endY - startY) / (endX - startX)
@@ -343,12 +440,59 @@ $(document).ready(function () {
         pointsQBez.push({ x: Ax, y: Ay });
         for (var i = 1; i < ptCount; i++) {
             var t = i / ptCount;
-
-            console.log(t)
             pointsQBez.push(getQuadraticCurvePoint(Ax, Ay, Cx, Cy, Bx, By, t))
         }
         pointsQBez.push({ x: Bx, y: By });
         return (pointsQBez);
     };
+
+    // line intercept math by Paul Bourke http://paulbourke.net/geometry/pointlineplane/
+    // Determine the intersection point of two line segments
+    // Return FALSE if the lines don't intersect
+    function line_intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+
+        // Check if none of the lines are of length 0
+        if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+            return false
+        }
+    
+        denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+        console.log(denominator)
+        // Lines are parallel
+        if (denominator === 0) {
+            return false
+        }
+    
+        let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
+        let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
+    
+        // is the intersection along the segments
+        if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+            return false
+        }
+    
+        // Return a object with the x and y coordinates of the intersection
+        let x = x1 + ua * (x2 - x1)
+        let y = y1 + ua * (y2 - y1)
+
+        // check if intersection is inside boundaries
+        if ((x > 0) && (x < width) && ((y > 0) && (y < height))){
+            // only return the intersection if it is inside of the curve
+            if ((x > x2) && (x > x4)){
+                if ((x2 > x1) && (x4 > x3)){
+                    return {x:x, y:y}
+                }else{
+                    return false
+                }
+            }else if ((x < x2) && (x < x4)){
+                if ((x2 < x1) && (x4 < x3)){
+                    return {x:x, y:y}
+                }else{
+                    return false
+                }
+            }
+            return false
+        }   
+    }
 
 });
